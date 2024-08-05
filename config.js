5de7bfeed6f01e6ed1cf78bc8874b83ca9c3fed5
savvycoop-dev <developer@savvy.coop>
2024-08-05 14:59:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const config = {
-  domainSource: "https://5a01a34e-e669-4654-9a30-49e0ca703474-staging.weweb-preview.io/", // Your WeWeb app preview link
+  domainSource: "https://www.madeeasy.ai/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/blog/[^/]+/[^/]+",
+          pattern: "/blog/[^/]+",
           metaDataEndpoint: "https://api.madeeasy.ai/api:3DvLmlut/blogs/{id}/metadata"
       }
       // Add more patterns and their metadata endpoints as needed
